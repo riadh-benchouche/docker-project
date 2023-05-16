4b30e84041dfe3c431e064fcf818ffaca57b1e61
riadh-fstck <r.benchouche1@gmail.com>
2023-05-16 13:50:34 +0200

    fix symfony and php problems

--- postgres/init.sql
+++ postgres/init.sql
@@ -5,13 +5,13 @@ CREATE TABLE todo (
     name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO todo (name) VALUES ('Faire les courses');
-INSERT INTO todo (name) VALUES ('Faire le ménage');
-INSERT INTO todo (name) VALUES ('Répondre aux e-mails');
-INSERT INTO todo (name) VALUES ('Préparer la réunion');
+INSERT INTO todo (name) VALUES ('Acheter du lait');
+INSERT INTO todo (name) VALUES ('Acheter du beurre');
+INSERT INTO todo (name) VALUES ('Acheter du fromage');
+INSERT INTO todo (name) VALUES ('Faire un sandwich');
+INSERT INTO todo (name) VALUES ('Manger le sandwich');
 INSERT INTO todo (name) VALUES ('Faire la vaisselle');
-INSERT INTO todo (name) VALUES ('Aller chez le médecin');
-INSERT INTO todo (name) VALUES ('Réserver le vol');
-INSERT INTO todo (name) VALUES ('Réparer la machine à laver');
-INSERT INTO todo (name) VALUES ('Préparer le dîner');
-INSERT INTO todo (name) VALUES ('Sortir les poubelles');
+INSERT INTO todo (name) VALUES ('Faire la sieste');
+INSERT INTO todo (name) VALUES ('Faire du sport');
+INSERT INTO todo (name) VALUES ('Faire du code');
+INSERT INTO todo (name) VALUES ('Faire du café');
