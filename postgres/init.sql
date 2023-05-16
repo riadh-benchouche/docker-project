04c62e6f43034e744052da7c3c3efac8986859c6
Riadh Benchouche <riadh@fstck.co>
2023-05-16 08:37:48 +0200

    wip

--- postgres/init.sql
+++ postgres/init.sql
@@ -15,13 +15,3 @@ INSERT INTO todo (name) VALUES ('Réserver le vol');
 INSERT INTO todo (name) VALUES ('Réparer la machine à laver');
 INSERT INTO todo (name) VALUES ('Préparer le dîner');
 INSERT INTO todo (name) VALUES ('Sortir les poubelles');
-
--- CREATE TABLE todo (
---     id SERIAL PRIMARY KEY,
---     name VARCHAR(255) NOT NULL,
---     completed BOOLEAN NOT NULL DEFAULT false
--- );
-
--- INSERT INTO todo (name, completed) VALUES ('Faire les courses', false);
--- INSERT INTO todo (name, completed) VALUES ('Faire le ménage', true);
--- INSERT INTO todo (name, completed) VALUES ('Répondre aux e-mails', false);
